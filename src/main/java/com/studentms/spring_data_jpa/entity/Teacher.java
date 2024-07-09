cc3dc6f8186f044df054286fe7c46f324adcd33e
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-10 00:50:50 +0530

    Created ontToMany relation between teacher and course

---
+++ src/main/java/com/studentms/spring_data_jpa/entity/Teacher.java
@@ -0,0 +1,34 @@
+package com.studentms.spring_data_jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Teacher {
+
+    @Id
+    @SequenceGenerator(name = "teacher_sequence", sequenceName = "teacher_sequence", allocationSize = 1)
+    @GeneratedValue(
+            generator = "teacher_sequence",
+            strategy = GenerationType.SEQUENCE
+    )
+    private Long teacherId;
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinColumn(
+            name = "teacher_id",
+            referencedColumnName = "teacherId"
+    )
+    private List<Course> courses;
+}
---
+++ src/main/java/com/studentms/spring_data_jpa/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.studentms.spring_data_jpa.repository;
+
+import com.studentms.spring_data_jpa.entity.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
