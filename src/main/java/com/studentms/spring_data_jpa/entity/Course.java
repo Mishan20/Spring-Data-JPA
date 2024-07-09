71b7f048e653b8d52d26e841ce14e45c1a1b03b0
Mishan20 <ishansenanayaka1@gmail.com>
2024-07-10 01:22:33 +0530

    Created manyToMany relation between student and course

--- src/main/java/com/studentms/spring_data_jpa/entity/Course.java
+++ src/main/java/com/studentms/spring_data_jpa/entity/Course.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Data
 @AllArgsConstructor
@@ -39,5 +42,26 @@ public class Course {
             referencedColumnName = "teacherId"
     )
     private Teacher teacher;
+
+    @ManyToMany(
+            cascade = CascadeType.ALL
+    )
+    @JoinTable(
+            name = "student_course",
+            joinColumns = @JoinColumn(
+                    name = "course_id",
+                    referencedColumnName = "courseId"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "student_id",
+                    referencedColumnName = "studentId"
+            )
+    )
+    private List<Student> students;
+
+    public void addStudents(Student student){
+        if(students == null) students = new ArrayList<>();
+        students.add(student);
+    }
 
 }
